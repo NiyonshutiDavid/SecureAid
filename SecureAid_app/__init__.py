82847ab0b46d08a6d5e958e808fb9506c305e8f3
NiyonshutiDavid <144002340+NiyonshutiDavid@users.noreply.github.com>
2024-07-29 14:23:24 +0200

    Update __init__.py

--- SecureAid_app/__init__.py
+++ SecureAid_app/__init__.py
@@ -4,10 +4,13 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///SecureAid.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///secureaid.db"  # Use secureaid.db for the database
+app.config['SECRET_KEY'] = 'your_secret_key'  # Set a secret key for session management
+
 db = SQLAlchemy(app)
-bcrypt= Bcrypt(app)
-login_manager= LoginManager(app)
-login_manager.login_view= "login"
-login_manager.login_message_category= "info"
-from SecureAid_app import routes
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = "login"
+login_manager.login_message_category = "info"
+
+from SecureAid_app import routes  # Import routes after initializing app and db
