ffff5ec04daa386d070ce2eb71f39fcfb85e5cb6
NiyonshutiDavid <144002340+NiyonshutiDavid@users.noreply.github.com>
2024-07-29 14:32:11 +0200

    Update form.py

--- SecureAid_app/form.py
+++ SecureAid_app/form.py
@@ -1,1 +1,21 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, IntegerField
+from wtforms.validators import DataRequired, Email, Length
 
+class RegistrationForm(FlaskForm):
+    first_name = StringField('First Name', validators=[DataRequired(), Length(max=20)])
+    last_name = StringField('Last Name', validators=[DataRequired(), Length(max=20)])
+    email = StringField('Email', validators=[DataRequired(), Email(), Length(max=40)])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=6)])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired()])
+    submit = SubmitField('Sign Up')
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Login')
+
+class DonationForm(FlaskForm):
+    donation_name = StringField('Donation Name', validators=[DataRequired(), Length(max=50)])
+    required_amount = IntegerField('Required Amount', validators=[DataRequired()])
+    submit = SubmitField('Donate')
